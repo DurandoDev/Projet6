d4f7ddf7f2617f6f8d29e9d24e53d218ea2e4ab4
DurandoDev <nat.durando@gmail.com>
2021-12-28 12:38:20 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApp.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyApp.class, args);
+	}
+
+}
