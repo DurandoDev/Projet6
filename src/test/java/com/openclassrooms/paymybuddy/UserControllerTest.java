b7f0d262e14ffbe22df456ff94850bbfa0b3091c
DurandoDev <nat.durando@gmail.com>
2022-09-16 16:55:15 +0200

    fixe

--- src/test/java/com/openclassrooms/paymybuddy/UserControllerTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/UserControllerTest.java
@@ -7,7 +7,7 @@ import com.openclassrooms.paymybuddy.repository.UserRepo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;Fic
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
